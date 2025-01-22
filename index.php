c25dcd9c55ffd4cd0263c74e965de4f5f8babd58
matytaxx <matiasquaz@gmail.com>
2025-01-22 15:42:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <div class="jumbotron">
-    <img class="card-img-right" src="img/logo.png" alt="" height="180" href="perfil.php">
+    
     <h1 class="display-3">Bienvenidos a Blockbusm:
         <?php echo $nombreUsuario ?>
     </h1>
