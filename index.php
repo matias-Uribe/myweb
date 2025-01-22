29bca76ff5cc33dc0586ef9c215abca079c40676
matytaxx <matiasquaz@gmail.com>
2025-01-22 16:02:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="img/logo.png">
-  <title>Blockusm</title>
+  <title>Matias Uribe</title>
 
   <link rel="stylesheet" href="./css/bootstrap.css" />
 </head>
