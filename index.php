8b73c45584440037c878ebbff039a67cfa795c79
matytaxx <matiasquaz@gmail.com>
2025-01-22 16:12:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -27,14 +27,14 @@
           <li class="nav-item">
             <a class="nav-link" href="peliculas.php">Peliculas</a>
           </li>
+
+          <h1 class="display-3">Bienvenidos a mi portafolio:</h1>
         </ul>
       </div>
     </div>
   </nav>
 
-<div class="jumbotron">
-    <h1 class="display-3">Bienvenidos a mi portafolio:</h1>
-</div>
+
 
 
 
